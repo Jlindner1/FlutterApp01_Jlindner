567fbab0bfa2f0b50e77aded58d86b35b654a2ff
Jlindner1 <46451910+Jlindner1@users.noreply.github.com>
2020-10-02 21:00:33 -0600

    alignment corrected and containers finished

--- lib/main.dart
+++ lib/main.dart
@@ -9,31 +9,121 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      backgroundColor: Colors.blueGrey,
-      appBar: AppBar(
-        title: Text("Row and Column Widgets"),
-        backgroundColor: Colors.blue,
-      ),
-      body: Row(
-        children: <Widget>[
-          Column(
-            children: <Widget>[
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(width: 3, color: Colors.black),
-                  color: Colors.orange,
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text("Row and Column Widgets"),
+          backgroundColor: Colors.blue,
+        ),
+        body: Center(
+            child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(width: 3, color: Colors.black),
+                    color: Colors.orange,
+                  ),
+                  height: 100.0,
+                  width: 100.0,
+                  //margin: EdgeInsets.all(10.0),
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Text("Container 1"),
+                  ),
                 ),
-                height: 100.0,
-                width: 100.0,
-              ),
-              Container(),
-            ],
-          ),
-          Column(),
-          Column(),
-        ],
+                Transform(
+                  alignment: Alignment.center,
+                  transform: Matrix4.rotationZ(0.785398),
+                  child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                      ),
+                      height: 100.0,
+                      width: 100.0,
+                      //margin: EdgeInsets.all(10.0),
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Text("Container 2"),
+                      )),
+                ),
+              ],
+            ),
+            Column(
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    color: Colors.yellow,
+                    height: 100.0,
+                    width: 100.0,
+                    margin: EdgeInsets.all(10.0),
+                    child: Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Text("Container 3"),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    color: Colors.blue,
+                    height: 100.0,
+                    width: 100.0,
+                    margin: EdgeInsets.all(10.0),
+                    child: Align(
+                      alignment: Alignment.centerRight,
+                      child: Text("Container 4"),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    width: 100,
+                    height: 100,
+                    child: CircleAvatar(
+                      backgroundColor: Colors.white,
+                      child: Center(
+                        child: CircleAvatar(
+                          backgroundColor: Colors.black,
+                          radius: 47,
+                          child: Align(
+                            alignment: Alignment.center,
+                            child: Text(
+                              "Container 5",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    width: 100,
+                    height: 100,
+                    color: Colors.red,
+                    child: Align(
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                        "Con 6",
+                        style: TextStyle(
+                          fontSize: 30,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        )),
       ),
-    ));
+    );
   }
 }
